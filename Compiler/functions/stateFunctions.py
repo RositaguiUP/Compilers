721c72d5002886d5b2ba2a4df411c12f1561f5fc
Rosita Aguirre Plascencia <0225352@up.edu.mx>
2023-05-23 00:19:24 -0600

    comments in!

--- Compiler/functions/stateFunctions.py
+++ Compiler/functions/stateFunctions.py
@@ -11,33 +11,59 @@ def checkGrammar(lexemas):
     lex = lexemas[0]
     actualState = State("Prgrm", 0, id)
     gramToComp = grams[actualState.gram][actualState.index]
-    res = checkLex(lex, 0, actualState, actualState, lexemas, gramToComp, False)
+    res = checkLex(lex, 0, actualState, actualState, lexemas, gramToComp, True)
     if res[0] >= 0:
         return 1
     return -1
 
 def checkLex(lex, i, actualState, parentState, lexemas, gramToComp, parentEmpty):
     errorMsg = ""
-    if isinstance(gramToComp, str):     # If is a string
+    if isinstance(gramToComp, str):                 # If is a string
         empt = False
         if gramToComp.startswith('[') and len(gramToComp) > 1:
             empt = True
             gramToComp = gramToComp[1:]
 
-        if lex[0] == gramToComp:
-            return i, errorMsg
+        if lex[0] == gramToComp:                    # Are the same word
+            return i, errorMsg, parentEmpty
         elif empt == True:
-            return -2, errorMsg
+            return -2, errorMsg, parentEmpty
         else:
             errorMsg = stringError(gramToComp)
-            return -1, errorMsg
+            return -1, errorMsg, parentEmpty
     elif isinstance(gramToComp, tuple):
-        empt = False                             # Could be empty
-        if gramToComp[1] == "e":
+        empt = False                                
+        if gramToComp[1] == "e":                    # Could be empty
             empt = True
 
-        if (gramToComp[0] == "cg"):              # If is a compuned grammar
-            for k in range(2, len(gramToComp)):
+        elif (gramToComp[0] == "t"):                # If is a token
+            if lex[1] == gramToComp[2]:             # Have same token
+                if i > 0:
+                    parentEmpty =  False
+                return i, errorMsg, parentEmpty
+            elif (empt == False):
+                return -1, errorMsg, parentEmpty
+            else:
+                return -2, errorMsg, parentEmpty
+            
+        elif (gramToComp[0] == "|"):                # One or another
+            for m in range(2, len(gramToComp)):     # Compare to all its possbile grammars
+                gramToCompAux = gramToComp[m]
+                if i >= len(lexemas):
+                    return checkEmpty(i, gramToCompAux)
+                lex = lexemas[i]
+                res = checkLex(lex, i, actualState, actualState, lexemas, gramToCompAux, parentEmpty)
+                if res[0] >= 0:                     # Once one is fine, finish successfuly
+                    i = res[0]
+                    parentEmpty = res[2]
+                    return i, errorMsg, parentEmpty
+            if (empt == False):
+                return -1, errorMsg, parentEmpty
+            else:
+                return -2, errorMsg, parentEmpty
+
+        if (gramToComp[0] == "cg"):                 # If is a compuned grammar
+            for k in range(2, len(gramToComp)):     # Compare to each of tis subgrammars
                 gramToCompAux =  gramToComp[k]
                 if i >= len(lexemas):
                     return checkEmpty(i, gramToCompAux)
@@ -45,22 +71,23 @@ def checkLex(lex, i, actualState, parentState, lexemas, gramToComp, parentEmpty)
                 res = checkLex(lex, i, actualState, actualState, lexemas, gramToCompAux, parentEmpty)
                 if res[0] == -1:
                     if empt == False:
-                        if (i != 0 and parentEmpty):
+                        if (i != 0 and not parentEmpty):
                             errorMsg = res[1]
                             print(errorMsg)
-                        return -1, errorMsg
+                        return -1, errorMsg, parentEmpty
                     else:
-                        return -2, errorMsg
+                        return -2, errorMsg, parentEmpty
                 elif res[0] >= 0:
                     i = res[0] + 1
+                    parentEmpty = res[2]
             i -= 1
-            return i, errorMsg
+            return i, errorMsg, parentEmpty
         
-        elif (gramToComp[0] == "g"):            # If is a grammar
+        elif (gramToComp[0] == "g"):                # If is a grammar
             actualState = State(gramToComp[2], parentState.parentId, parentState.index+1)
             parentState.substates.append(actualState)
             parentState.substateStart = True
-            while(True):
+            while(True):                            # Compare to each element
                 gramToCompAux = grams[actualState.gram][actualState.index]
                 if i >= len(lexemas):
                     return checkEmpty(i, gramToCompAux)
@@ -68,41 +95,21 @@ def checkLex(lex, i, actualState, parentState, lexemas, gramToComp, parentEmpty)
                 res = checkLex(lex, i, actualState, actualState, lexemas, gramToCompAux, parentEmpty)
                 if res[0] == -1:
                     if empt == False:
-                        return -1, errorMsg
+                        return -1, errorMsg, parentEmpty
                     else:
-                        return -2, errorMsg
+                        return -2, errorMsg, parentEmpty
                 else:
                     gramLen = len(grams[actualState.gram])
                     actualState.addIndex()
                     if res[0] != -2:
                         i = res[0] + 1
-                if actualState.index == gramLen:
+                        parentEmpty = res[2]
+                if actualState.index == gramLen:    # Finish when ends checking all the grammar elements
                     actualState.index -= 1
                     i -= 1
-                    return i, errorMsg
+                    return i, errorMsg, parentEmpty
 
-        elif (gramToComp[0] == "t"):           # If is a token
-            if lex[1] == gramToComp[2]:
-                return i, errorMsg
-            elif (empt == False):
-                return -1, errorMsg
-            else:
-                return -2, errorMsg
-            
-        elif (gramToComp[0] == "|"):
-            for m in range(2, len(gramToComp)):
-                gramToCompAux = gramToComp[m]
-                if i >= len(lexemas):
-                    return checkEmpty(i, gramToCompAux)
-                lex = lexemas[i]
-                res = checkLex(lex, i, actualState, actualState, lexemas, gramToCompAux, parentEmpty)
-                if res[0] >= 0:
-                    i = res[0]
-                    return i, errorMsg
-            if (empt == False):
-                return -1, errorMsg
-            else:
-                return -2, errorMsg
+        
     
 def checkEmpty(i, gramToComp):
     errorMsg = ""
@@ -114,9 +121,9 @@ def checkEmpty(i, gramToComp):
         if gramToComp[1] == "e":
             empt = True
     if empt:
-        return i, errorMsg
+        return i, errorMsg, False
     else:
-        return -1, errorMsg
+        return -1, errorMsg, False
     
 
     # i = 0
